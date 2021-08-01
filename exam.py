0e747b4713b45496aef05eb9e1496a549401829e
Jean-philippe Martel <jemartel@c2r5p3.42quebec.com>
2021-07-31 20:22:39 -0400

    clean up  still nead some more feature

--- exam.py
+++ exam.py
@@ -5,12 +5,11 @@ import random
 def pathandler(number):
     output = subprocess.run(['ls',"./exams/0" + str(number) ],capture_output=True, text=True)
     return(output)
- ##working now 
+
 def subjecthandle(output,number):
     lst = output.stdout.split("\n")
     rdm = random.randrange(len(lst ) - 1)
     string = "exams/0" + str(number) + "/" + lst[rdm] + "/" + "subject.en.txt"
-    print(rdm)
     return(string)
 
 def addpath(path):
@@ -18,5 +17,4 @@ def addpath(path):
     return (output.stdout)
 location = random.randrange(0,5)
 output = pathandler(location)
-print(location)
-print(  addpath(subjecthandle(pathandler(location),location)).stdout)
+print(addpath(subjecthandle(pathandler(location),location)).stdout)
